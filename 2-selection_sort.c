a8866d9aba885a0cba2815fc456fc7ea064adbb3
Michael Nyasera <117773134+MichaelN-SE@users.noreply.github.com>
2023-04-24 08:56:44 +0300

    Create 2-selection_sort.c
    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_int - sort two by swap
+ * @a: first integer.
+ * @b: second integer.
+ */
+
+void swap_int(int *a,  int *b)
+{
+	int temp;
+
+	temp =  *a;
+	*a =  *b;
+	*b =  temp;
+}
+
+/**
+ * selection_sort - selection sorting of arrays
+ * @array: arrays to sort
+ * @size: size of arrays
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+
+	if (array  ==  NULL || size < 2)
+
+		return;
+	for (i =  0; i < size; i++)
+	{
+		min =  i;
+		for (j = (i + 1); j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min !=  i)
+		{
+			swap_int(&array[i], &array[min]);
+			print_array(array, size);
+		}
+	}
+}
